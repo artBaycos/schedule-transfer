679afdb95e774d7fe664f90191a60551055d09b5
Arthur <arthur.pinesi@outlook.com>
2025-01-30 04:01:42 -0300

    feat: Inicializar o repo

---
+++ schedule/src/main/java/com/teste/schedule/TransferSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.teste.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferSchedulerApplication.class, args);
+	}
+
+}
---
+++ schedule/src/test/java/com/teste/schedule/TransferSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
